5715665b0114a4d17ee6034971879710ee96e1fb
YiSha <yswhynot@gmail.com>
2016-10-11 21:19:38 +0800

    update

---
+++ js/portfolio_item.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    // $('.materialboxed').parent().removeClass('material-placeholder');
+    var cw = $('.p-item-img').width();
+	$('.p-item-img').css({'height':cw+'px'});
+  });
+
