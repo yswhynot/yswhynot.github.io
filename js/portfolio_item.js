ae8e50334071f22c5599b99ad74107192456261c
YiSha <yswhynot@gmail.com>
2016-10-12 21:25:44 +0800

    update

--- js/portfolio_item.js
+++ js/portfolio_item.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
-    $('.materialboxed').materialbox();
+	$('.materialboxed').materialbox();
+	$('.tooltipped').tooltip({delay: 50});
     // $('.materialboxed').parent().removeClass('material-placeholder');
-    var cw = $('.p-item-img').width();
-	$('.p-item-img').css({'height':cw+'px'});
-  });
+    updateImgs();
+});
 
+$(window).on("load", function() {
+    updateImgs();
+});
+
+function updateImgs () {
+	var cw = $('.p-item-img').width();
+    $('.p-item-img').css('height', cw + 'px');
+    console.log('w: ' + cw);
+    // $('.p-item-img-h img').css('height', cw + 'px');
+
+    $( ".p-item-img").each(function( index ) {
+    	var img = $(this).find('img')[0];
+    	var imgw = img.clientWidth;
+    	var imgh = img.clientHeight;
+    	if(imgw > imgh)
+    		$(this).addClass('p-item-img-h');
+    	else
+    		$(this).addClass('p-item-img-v');
+    });
+}
