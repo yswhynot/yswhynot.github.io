36e46af5022925f327fd0d9f75d46c236849d7e2
YiSha <yswhynot@gmail.com>
2026-05-12 17:56:32 -0700

    update

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,11 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  // About-page news block; PurgeCSS can drop these rules after `jekyll build` so scrolling works locally but not on GH Pages.
+  safelist: [
+    "about-announcements",
+    "news-from-data",
+    "news-from-data__viewport",
+    "news-from-data__viewport--limited",
+  ],
 };
